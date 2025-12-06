68a1e0b26b9e3d06d188656b1868bcb19e9387b5
Meena K <102808984+ELANGOMEENA@users.noreply.github.com>
2025-12-06 19:01:54 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -1,3 +1,3 @@
-def call(servername,artifact_name) {
-   echo sshPublisher(publishers: [sshPublisherDesc(configName: ${servername}, transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: 'target', sourceFiles: 'target/${artifact_name}.war')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
+def call(artifact_name) {
+   echo sshPublisher(publishers: [sshPublisherDesc(configName: 'tomcat', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: 'target', sourceFiles: 'target/${artifact_name}.war')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
 }
