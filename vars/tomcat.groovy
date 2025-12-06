4bd44075284dd2907acaf49adb1bfbba3ea062ba
Meena K <102808984+ELANGOMEENA@users.noreply.github.com>
2025-12-06 18:55:05 +0530

    Refactor call function in tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -1,29 +1,3 @@
-def call(String servername, String artifact_name) {
-
-    sshPublisher(
-        publishers: [
-            sshPublisherDesc(
-                configName: servername,
-                transfers: [
-                    sshTransfer(
-                        cleanRemote: false,
-                        excludes: '',
-                        execCommand: '',
-                        execTimeout: 120000,
-                        flatten: false,
-                        makeEmptyDirs: false,
-                        noDefaultExcludes: false,
-                        patternSeparator: '[, ]+',
-                        remoteDirectory: '',
-                        remoteDirectorySDF: false,
-                        removePrefix: 'target',
-                        sourceFiles: "target/${artifact_name}.war"
-                    )
-                ],
-                usePromotionTimestamp: false,
-                useWorkspaceInPromotion: false,
-                verbose: false
-            )
-        ]
-    )
+def call(servername,artifact_name) {
+   sshPublisher(publishers: [sshPublisherDesc(configName: ${servername}, transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: 'target', sourceFiles: 'target/${artifact_name}.war')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
 }
