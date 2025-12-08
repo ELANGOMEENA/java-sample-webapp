580edb05550e6702daa84b3a4026276a568724dc
Meena K <102808984+ELANGOMEENA@users.noreply.github.com>
2025-12-08 23:04:23 +0530

    Update Tomcat shutdown and startup script paths

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -1,4 +1,4 @@
 def call(servername,artifact_name){
- sshPublisher(publishers: [sshPublisherDesc(configName: "${servername}", transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '''/opt/tomcat/bin/shutdown.sh
-/opt/tomcat/bin/startup.sh''', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: 'target', sourceFiles: 'target/${artifact_name}.war')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)]) 
+ sshPublisher(publishers: [sshPublisherDesc(configName: "${servername}", transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '''/opt/tomcat1/bin/shutdown.sh
+/opt/tomcat1/bin/startup.sh''', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: 'target', sourceFiles: 'target/${artifact_name}.war')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)]) 
 }
